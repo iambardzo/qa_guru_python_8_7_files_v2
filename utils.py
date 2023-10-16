6d6993c8818ed506cff14733421f61c5adb5ba36
chernenko_mi <chernenko_mi@magnit.ru>
2023-10-16 18:20:57 +0300

    1

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ RESOURCES_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
 TMP_PATH = os.path.join(PROJECT_ROOT_PATH, 'tmp')
 ZIP_PATH = os.path.join(TMP_PATH, 'archive.zip')
 
-PDF_FILE = 'filepdf.pdf'
-TXT_FILE = 'filetxt.txt'
-XLS_FILE = 'filexls.xls'
-XLSX_FILE = 'filexlsx.xlsx'
+PDF_FILE = 'HelloPDF.pdf'
+TXT_FILE = 'TextDocument.txt'
+XLS_FILE = 'xlsExcel.xls'
+XLSX_FILE = 'xlsxExcel.xlsx'
