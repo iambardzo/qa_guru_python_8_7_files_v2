a319d7ab510994fe82c511a67f03594205b06e74
Max Chernenko <145380709+iambardzo@users.noreply.github.com>
2023-10-16 17:44:37 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+import os
+
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
+TMP_PATH = os.path.join(PROJECT_ROOT_PATH, 'tmp')
+ZIP_PATH = os.path.join(TMP_PATH, 'archive.zip')
+
+PDF_FILE = 'filepdf.pdf'
+TXT_FILE = 'filetxt.txt'
+XLS_FILE = 'filexls.xls'
+XLSX_FILE = 'filexlsx.xlsx'
